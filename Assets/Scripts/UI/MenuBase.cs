e0dd89700dde6432c83d006d115cea20fa75e998
Jack Stevenson <jpstevenson.avi@gmail.com>
2025-09-29 17:55:02 -0700

    Updatede stuff

---
+++ Assets/Scripts/UI/MenuBase.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class MenuBase : MonoBehaviour {
+    private RectTransform _rect;
+    
+    public Vector2 Position;
+    
+    
+    
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
